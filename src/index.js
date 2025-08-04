0fd5c67c9f926d979bc2d5342de097212eb9a89e
Grimcat07 <166890725+Grimcat07@users.noreply.github.com>
2025-08-04 19:57:02 +0530

    completed image carousel

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import "./styles.css";
-
+let currentSlide=0;
+let dots=new Array()
 const pictureFrame = document.querySelector(".pictureFrame");
 const container = pictureFrame.querySelector(".container");
 console.log(container)
 const slides = Array.from(container.querySelectorAll(".slides"));
 const nextButton=document.querySelector(".nextSlide")
 const prevButton=document.querySelector(".prevSlide")
+const navigation=document.querySelector(".navigation")
 function init() {
   slides.forEach((slide) => {
     slide.style.display = "none";
-    slides[0].style.display="block"
+    
   });
+  slides[0].style.display="block"
+  attachNavigation();
 }
 
 init();
 
-let currentSlide=0;
 
 function nextSlide(){
     hideSlide(currentSlide)
@@ -23,16 +26,16 @@ function nextSlide(){
     if(currentSlide>=slides.length){
         currentSlide=0
     }
-    
-    showSlide(currentSlide)
-    
+    showSlide(currentSlide)   
 }
 function showSlide(slideno){
     slides[slideno].style.display="block"
+    dots[currentSlide].classList.add("active")
 }
 
 function hideSlide(slideno){
     slides[slideno].style.display="none"
+    dots[currentSlide].classList.remove("active")
 }
 function prevSlide(){
     hideSlide(currentSlide)
@@ -44,3 +47,22 @@ function prevSlide(){
 }
 nextButton.addEventListener("click",nextSlide)
 prevButton.addEventListener("click",prevSlide)
+
+function attachNavigation(){
+    slides.forEach((_,index)=>{
+        const navDot=document.createElement("span")
+        navDot.className="navDot"
+        if(index===currentSlide){
+            navDot.classList.add("active")
+        }
+        dots.push(navDot)
+        navDot.addEventListener("click",()=>goTo(index))
+        navigation.appendChild(navDot)
+    })
+}
+function goTo(index)
+{
+    hideSlide(currentSlide)
+    currentSlide=index
+    showSlide(index)
+}
